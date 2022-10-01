0faab2d300c268b9e848605556476b1f3b67e26d
Andi Nawfal <nawfaldzikra1611@gmail.com>
2022-10-01 16:35:00 +0700

    first commit

---
+++ dist/js/script.js
@@ -0,0 +1,16 @@
+// // Navbar Fixed
+// window.onscroll = function () {
+//   const header = document.querySelector('navbar');
+//   const fixedNav = header.offsetTop;
+//   const toTop = document.querySelector('#to-top');
+
+//   if (window.pageYOffset > fixedNav) {
+//     header.classList.add('navbar-fixed');
+//     toTop.classList.remove('hidden');
+//     toTop.classList.add('flex');
+//   } else {
+//     header.classList.remove('navbar-fixed');
+//     toTop.classList.remove('flex');
+//     toTop.classList.add('hidden');
+//   }
+// };
